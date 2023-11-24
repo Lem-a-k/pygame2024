a0e88404a44b7c6db01a4c7a717f486f28bceb58
west1 <mstal64@yandex.ru>
2023-11-24 17:11:33 +0400

    moving square

--- main.py
+++ main.py
@@ -1,27 +1,46 @@
+import random
+
 import pygame
-
-
-def draw(screen):
-    screen.fill((0, 0, 0))
-    font = pygame.font.Font(None, 50)
-    text = font.render("Hello, Pygame!", True, (100, 255, 100))
-    text_x = width // 2 - text.get_width() // 2
-    text_y = height // 2 - text.get_height() // 2
-    text_w = text.get_width()
-    text_h = text.get_height()
-    screen.blit(text, (text_x, text_y))
-    pygame.draw.rect(screen, (0, 255, 0), (text_x - 10, text_y - 10,
-                                           text_w + 20, text_h + 20), 1)
-
 
 if __name__ == '__main__':
     pygame.init()
-    size = width, height = 800, 600
+    size = width, height = 800, 400
     screen = pygame.display.set_mode(size)
 
-    draw(screen)
-    pygame.display.flip()
-    while pygame.event.wait().type != pygame.QUIT:
-        pass
-
+    running = True
+    fps = 30
+    clock = pygame.time.Clock()
+    MY_EVENT = pygame.USEREVENT + 1
+    pygame.time.set_timer(MY_EVENT, 1000)
+    special_color = 0
+    pos = [10, 10]
+    while running:
+        # внутри игрового цикла ещё один цикл
+        # приема и обработки сообщений
+        for event in pygame.event.get():
+            # при закрытии окна
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    pos[0] += 10
+                elif event.key == pygame.K_LEFT:
+                    pos[0] -= 10
+                elif event.key == pygame.K_UP:
+                    pos[1] -= 10
+                elif event.key == pygame.K_DOWN:
+                    pos[1] += 10
+            if event.type == MY_EVENT:
+                screen.fill((0, 0, 0),
+                            (0, 0, width, height))
+                color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+                for i in range(100):
+                    screen.fill(color,
+                                (random.random() * width,
+                                 random.random() * height, 5, 5))
+        # обновление экрана
+        special_color = (special_color + 1) % 256
+        screen.fill((special_color, special_color, special_color), (*pos, 50, 50))
+        pygame.display.flip()
+        clock.tick(fps)
     pygame.quit()
