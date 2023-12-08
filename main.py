0219e6aca7cac6e9b7159d411901d19f76fdbf36
west1 <mstal64@yandex.ru>
2023-12-08 17:36:23 +0400

    Sprites and Groups (Cars)

--- main.py
+++ main.py
@@ -1,154 +1,80 @@
+import os
+import sys
 import random
 from copy import deepcopy
 from itertools import product
 
 import pygame
 
-
-class Board:
-    # создание поля
-    def __init__(self, board_width, board_height):
-        self.board_updating = False
-        self.width = board_width
-        self.height = board_height
-        self.board = [[0 if random.randrange(5) else 1 for _ in range(self.width)]
-                      for _ in range(self.height)]
-        self.num_nei = [[None] * self.width for _ in range(self.height)]
-        self.shown = [[False] * self.width for _ in range(self.height)]
-        # значения по умолчанию
-        self.left = 50
-        self.top = 10
-        self.cell_size = 30
-
-        self.font = pygame.font.Font(None, 20)
-
-    # настройка внешнего вида
-    def set_view(self, left, top, cell_size):
-        self.left = left
-        self.top = top
-        self.cell_size = cell_size
-
-    def render(self, screen):
-        for i in range(self.height):
-            for j in range(self.width):
-                r = (self.left + j * self.cell_size,
-                     self.top + i * self.cell_size,
-                     self.cell_size, self.cell_size)
-                if self.board[i][j] == 1:
-                    pygame.draw.rect(screen, (0, 255, 0), r)
-                if self.num_nei[i][j] is not None and self.shown[i][j]:
-                    digit = self.font.render(str(self.num_nei[i][j]),
-                                             True, (100, 100, 255))
-                    screen.blit(digit, (r[0] + 2, r[1] + 2))
-
-                pygame.draw.rect(screen, (255, 255, 255), r, 1)
-
-    def get_cell(self, mouse_pos):
-        row = (mouse_pos[1] - self.top) // self.cell_size
-        col = (mouse_pos[0] - self.left) // self.cell_size
-        return (row, col) if (0 <= row < self.height and
-                              0 <= col < self.width) else None
-
-    def on_click(self, cell, value):
-        self.board[cell[0]][cell[1]] = value
-
-    def process_click(self, mouse_pos, value):
-        cell = self.get_cell(mouse_pos)
-        if cell is not None:
-            self.on_click(cell, value)
-
-    def open(self, mouse_pos):
-        cell = self.get_cell(mouse_pos)
-        if cell is not None:
-            self.shown[cell[0]][cell[1]] = True
-            if self.num_nei[cell[0]][cell[1]] == 0:
-                self.open_nei_shortest(cell[0], cell[1])
-
-    def open_nei_shortest(self, i, j):  # обход в ширину
-        cur_gen = [(i, j)]
-        while cur_gen:
-            next_gen = []
-            for i, j in cur_gen:
-                for di, dj in product((-1, 0, 1), repeat=2):
-                    if di == dj == 0:
-                        continue
-                    ni, nj = i + di, j + dj
-                    if 0 <= ni < self.height and 0 <= nj < self.width:
-                        if not self.shown[ni][nj]:
-                            self.shown[ni][nj] = True
-                            if self.num_nei[ni][nj] == 0:
-                                next_gen.append((ni, nj))
-            cur_gen = next_gen
+pygame.init()
+size = width, height = 500, 500
+screen = pygame.display.set_mode(size)
 
 
-    def open_nei(self, i, j):  # обход в глубину
-        for di, dj in product((-1, 0, 1), repeat=2):
-            if di == dj == 0:
-                continue
-            ni, nj = i + di, j + dj
-            if 0 <= ni < self.height and 0 <= nj < self.width:
-                if not self.shown[ni][nj]:
-                    self.shown[ni][nj] = True
-                    if self.num_nei[ni][nj] == 0:
-                        self.open_nei(ni, nj)
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        # image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+class Car(pygame.sprite.Sprite):
+    image = load_image("car_tr.png")
+    move = 10
+
+    def __init__(self, *group):
+        super().__init__(*group)
+        self.image = Car.image
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(width - self.rect.width)
+        self.rect.y = random.randrange(height - self.rect.height)
 
     def update(self):
-        new_board = deepcopy(self.board)
-        for i in range(self.height):
-            for j in range(self.width):
-                neighbours = 0
-                for di, dj in product((-1, 0, 1), repeat=2):
-                    if di == dj == 0:
-                        continue
-                    ni, nj = i + di, j + dj
-                    if 0 <= ni < self.height and 0 <= nj < self.width:
-                        neighbours += self.board[ni][nj]
-                self.num_nei[i][j] = neighbours
-                if neighbours <= 1 or neighbours > 3:
-                    new_board[i][j] = 0
-                elif neighbours == 3:
-                    new_board[i][j] = 1
-        self.board = new_board
+        self.rect = self.rect.move(random.randrange(3) - 1, 0)
+
+    def process_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN and \
+                self.rect.collidepoint(event.pos):
+            self.rect = self.rect.move(0, Car.move * (1 if random.randrange(2) else -1))
+
+
+class CarsGroup(pygame.sprite.Group):
+    def process_event(self, event):
+        for sprite in self.sprites():
+            sprite.process_event(event)
 
 
 if __name__ == '__main__':
-    pygame.init()
-    size = width, height = 900, 600
-    screen = pygame.display.set_mode(size)
+    all_sprites = CarsGroup()
+
+    for _ in range(4):
+        Car(all_sprites)
 
     running = True
     fps = 30
     clock = pygame.time.Clock()
-    BOARD_UPDATE = pygame.USEREVENT + 1
 
-    state = None
-    board = Board(40, 25)
-    board.set_view(10, 10, 20)
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            elif event.type == BOARD_UPDATE:
-                board.update()
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1:
-                    state = 1
-                elif event.button == 3:
-                    state = 0
-                elif event.button == 2:
-                    board.open(event.pos)
-            elif event.type == pygame.MOUSEBUTTONUP:
-                state = None
-            elif event.type == pygame.MOUSEMOTION:
-                if state is not None:
-                    board.process_click(event.pos, state)
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    pygame.time.set_timer(BOARD_UPDATE, 0 if board.board_updating else 1000)
-                    board.board_updating = not board.board_updating
+                all_sprites.process_event(event)
+
         # обновление экрана
-        screen.fill((0, 0, 0))
-        board.render(screen)
+        screen.fill((10, 150, 10))
+        all_sprites.update()
+        all_sprites.draw(screen)
         pygame.display.flip()
+
         clock.tick(fps)
     pygame.quit()
